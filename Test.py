8091237def4e62602d8409dcde95530b1c13a4a0
ARKA PRATIM GHOSH <arkapratimghosh2002@gmail.com>
2025-04-24 13:57:30 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -13,7 +13,7 @@ generation_config = {
 }
 
 gemini_model = genai.GenerativeModel(
-    model_name="gemini-2.0-flash",
+    model_name="gemini-2.5-flash",
     generation_config=generation_config,
 )
 
